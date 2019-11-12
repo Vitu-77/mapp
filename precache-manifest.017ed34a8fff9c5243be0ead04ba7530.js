1625648d15e4c3886529e152a6eb67f46e091e0b
Vitu Silva <marcelo.victor05@gmail.com>
2019-11-11 23:14:52 -0300

    v1.0.0

---
+++ precache-manifest.017ed34a8fff9c5243be0ead04ba7530.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98095a611c4a1a60a5481eabe813fa89",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c26685c8c9bf1e4401fe",
+    "url": "/static/css/2.a9fe6764.chunk.css"
+  },
+  {
+    "revision": "d5589d7a64f10a8edc54",
+    "url": "/static/css/main.444a5b09.chunk.css"
+  },
+  {
+    "revision": "c26685c8c9bf1e4401fe",
+    "url": "/static/js/2.b91ae59c.chunk.js"
+  },
+  {
+    "revision": "d5589d7a64f10a8edc54",
+    "url": "/static/js/main.648a413d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1c767467731adaa20de24dc769e297c8",
+    "url": "/static/media/HomeBackground.1c767467.svg"
+  },
+  {
+    "revision": "8e34dcc9fab79011f4073e27bb9fd2b1",
+    "url": "/static/media/Logo.8e34dcc9.svg"
+  }
+]);
